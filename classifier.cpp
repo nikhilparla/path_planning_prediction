027e23785d8ee6c2fec8e777e64ed76946f9215e
nikhilparla <nikhilp.bits@gmail.com>
2020-02-09 02:03:59 -0500

    Added the means and std devs of a few feature/lable pairs

--- classifier.cpp
+++ classifier.cpp
@@ -2,10 +2,13 @@
 #include <math.h>
 #include <string>
 #include <vector>
+#include<iostream>
+
 
 using Eigen::ArrayXd;
 using std::string;
 using std::vector;
+using namespace std;
 
 // Initializes GNB
 GNB::GNB() {
@@ -32,6 +35,129 @@ void GNB::train(const vector<vector<double>> &data,
    *
    * TODO: Implement the training function for your classifier.
    */
+
+  // find mean and std_dev for each lable/feature pair
+  // we have 4 features and 3 labels == 12 means and 12 std devs
+  // so 24 variables in total if brute forcing
+  // starting with mean for s/left feature lable
+  double sum_s = 0;
+  double sum_d = 0;
+  double sum_sdot = 0;
+  double sum_ddot = 0;
+  double mean_s = 0;
+  double mean_d = 0;
+  double mean_sdot = 0;
+  double mean_ddot = 0;
+  
+double mean_left_s;
+double mean_left_d;
+double mean_left_s_dot;
+double mean_left_d_dot;
+ 
+double mean_keep_s;
+double mean_keep_d;
+double mean_keep_s_dot;
+double mean_keep_d_dot;
+ 
+double mean_right_s;
+double mean_right_d;
+double mean_right_s_dot;
+double mean_right_d_dot;
+ 
+double mean_left_s;
+double mean_left_d;
+double mean_left_s_dot;
+double mean_left_d_dot;
+ 
+double mean_left_s;
+double mean_left_d;
+double mean_left_s_dot;
+double mean_left_d_dot;
+ 
+double stddev_keep_s;
+double stddev_keep_d;
+double stddev_keep_s_dot;
+double stddev_keep_d_dot;
+ 
+double stddev_right_s;
+double stddev_right_d;
+double stddev_right_s_dot;
+double stddev_right_d_dot;
+ 
+double stddev_left_s;;
+double stddev_left_d;
+double stddev_left_s_dot;
+double stddev_left_d_dot;
+
+
+
+  printf("Size of data0 is %lu \n", data[0].size()) ;
+  printf("The first value is %f \n", data[0][0]);
+  
+  vector<double>::const_iterator it;
+  cout << "The vector1 elements are: ";
+  for ( it = data[0].begin(); it != data[0].end(); ++it)
+    cout << *it << " "<<endl; // printing the values of v vector
+
+  vector<vector<double>>::const_iterator itt;
+  vector<double>::const_iterator obs;
+  // for ( itt = data.begin(); itt != data.end(); ++itt)
+
+  for(int i=0; i<data.size(); i++)
+  {
+    sum_s += data[i][0];
+    sum_d += data[i][1];
+    sum_sdot += data[i][2];
+    sum_ddot += data[i][3];
+  }
+  mean_s = sum_s/data.size();
+  mean_d = sum_d/data.size();
+  mean_sdot = sum_sdot/data.size();
+  mean_ddot = sum_ddot/data.size();
+
+  cout << "sum of s values = "  << sum_s<< endl;
+  cout << "Mean of s values = "  << mean_s<< endl;
+  cout << "sum of d values = "  << sum_d<< endl;
+  cout << "Mean of d values = "  << mean_d<< endl;
+  cout << "sum of sdot values = "  << sum_sdot<< endl;
+  cout << "Mean of sdot values = "  << mean_sdot<< endl;
+  cout << "sum of ddot values = "  << sum_ddot<< endl;
+  cout << "Mean of ddot values = "  << mean_ddot<< endl;
+
+  for(int i=0; i<labels.size(); i++)
+  {
+    int lab;
+    if(labels[i].compare("left"))
+      lab = 0;
+    else if(labels[i].compare("keep"))
+      lab = 1;
+    else if(labels[i].compare("right"))
+      lab = 2;
+    else
+      ;
+
+    switch(lab)
+    {
+    case 0: // left
+    {
+      mean_left_s;
+      mean_left_d;
+      mean_left_s_dot;
+      mean_left_d_dot;
+      }
+    
+    }
+
+  // double sum = std::accumulate(data.begin()[0], data.end()[1], 0.0);
+  // double mean = sum / v.size();
+
+  // std::vector<double> diff(v.size());
+  // std::transform(v.begin(), v.end(), diff.begin(),
+  //               std::bind2nd(std::minus<double>(), mean));
+  // double sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+  // double stdev = std::sqrt(sq_sum / v.size())
+  
+
   
 }
 
--- classifier.h
+++ classifier.h
@@ -3,7 +3,7 @@
 
 #include <string>
 #include <vector>
-#include "Dense"
+#include "eigen3/Eigen/Dense"
 
 using Eigen::ArrayXd;
 using std::string;
